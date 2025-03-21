143c87590ba81647ad477e31a97f40f6bda0e3e2
maxvantisio <brownmh3@appstate.edu>
2025-03-21 12:49:07 -0400

    Created Coin constructor

--- src/main/Coin.java
+++ src/main/Coin.java
@@ -1,3 +1,17 @@
 public class Coin {
-
+	public void Coin()
+	{
+		this.value = 0.0;
+		this.commonName = "";
+		this.frontMotto = "";
+		this.manufactureYear = 0;
+		this.frontImage = "";
+		this.backImage = "";
+		this.backMotto = "";
+		this.frontLabel = "";
+		this.backLabel = "";
+		this.valueDescription = "";
+		this.ridgedEdge = false;
+		this.metallurgy = "";
+	}
 }
