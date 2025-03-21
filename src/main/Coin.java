a7ce6c1d20bd1aa27d89ae4568e69b6638de041f
wickedworldstudios <joshjonesdevelopment@gmail.com>
2025-03-21 12:48:07 -0400

    Added properties to coin.java

--- src/main/Coin.java
+++ src/main/Coin.java
@@ -1,3 +1,16 @@
-public class Coin {
 
+public class Coin {
+    public double value;
+    public String commonName;
+    public String frontMotto;
+    public int manufactureYear;
+    public String frontImage;
+    public String backImage;
+    public String backMotto;
+    public String frontLabel;
+    public String backLabel;
+    public String valueDescription;
+    public boolean ridgedEdge;
+    public String metallurgy;
 }
+
