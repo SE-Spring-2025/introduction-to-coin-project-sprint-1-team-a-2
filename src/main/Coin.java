7b327981d625a0a16fe7e958fea6c886b7f3e28a
maxvantisio <brownmh3@appstate.edu>
2025-03-24 12:03:13 -0400

    created second constructor

--- src/main/Coin.java
+++ src/main/Coin.java
@@ -1,3 +1,11 @@
+/**
+* Class that creates coin objects based on what 
+* data (if any) is passed to methods
+*
+* @author  Maxwell Brown
+* @author  Gabriel Basham
+* @author  Josh Jones
+*/
 
 public class Coin {
     public double value;
@@ -13,9 +21,9 @@ public class Coin {
     public boolean ridgedEdge;
     public String metallurgy;
 	
-	public void Coin()
+	public Coin()
 	{
-		this.value = 0.0;
+		value = 0.0;
 		this.commonName = "";
 		this.frontMotto = "";
 		this.manufactureYear = 0;
@@ -27,5 +35,91 @@ public class Coin {
 		this.valueDescription = "";
 		this.ridgedEdge = false;
 		this.metallurgy = "";
+	}
+
+	/**
+	 * Creates a coin object based on the value passed in
+	 * 
+	 * @param value	- numerical value that indicates what coin should be created
+	 */
+	public Coin(double value)
+	{
+		//check if coin should be a penny
+		if (Double.compare(value, 0.01) == 0)
+		{
+			this.value = value;
+			this.commonName = "Penny";
+			this.frontImage = "Abe Lincoln";
+			this.backImage = "Lincoln Memorial";
+			this.valueDescription = "One cent";
+			this.ridgedEdge = false;
+			this.metallurgy = "Copper";
+		}
+		//check if coin should be a nickel
+		if (Double.compare(value, 0.05) == 0)
+		{
+			this.value = value;
+			this.commonName = "Nickel";
+			this.frontImage = "Thomas Jefferson";
+			this.backImage = "Jefferson Memorial";
+			this.valueDescription = "Five cents";
+			this.ridgedEdge = false;
+			this.metallurgy = "Cupro-Nickel";
+		}
+		//check if coin should be a dime
+		if (Double.compare(value, 0.10) == 0)
+		{
+			this.value = value;
+			this.commonName = "Dime";
+			this.frontImage = "Franklin D. Roosevelt";
+			this.backImage = "Olive branch, torch, and oak branch";
+			this.valueDescription = "Ten cents";
+			this.ridgedEdge = true;
+			this.metallurgy = "Cupro-Nickel";
+		}
+		//check if coin should be a quarter
+		if (Double.compare(value, 0.25) == 0)
+		{
+			this.value = value;
+			this.commonName = "Quarter";
+			this.frontImage = "George Washington";
+			this.backImage = "Eagle";
+			this.valueDescription = "25 cents";
+			this.ridgedEdge = true;
+			this.metallurgy = "Cupro-Nickel";
+		}
+		//check if coin should be a half dollar
+		if (Double.compare(value, 0.50) == 0)
+		{
+			this.value = value;
+			this.commonName = "Half Dollar";
+			this.frontImage = "John F. Kennedy";
+			this.backImage = "Presidential Seal";
+			this.valueDescription = "50 cents";
+			this.ridgedEdge = true;
+			this.metallurgy = "Cupro-Nickel";
+		}
+		//check if coin should be a dollar
+		if (Double.compare(value, 1.0) == 0)
+		{
+			this.value = value;
+			this.commonName = "Dollar";
+			this.frontImage = "Sacagawea Dollar";
+			this.backImage = "Eagle";
+			this.valueDescription = "100 cents";
+			this.ridgedEdge = true;
+			this.metallurgy = "Manganese Brass";
+		}
+
+		this.manufactureYear = 2025;
+
+		this.frontMotto = "IN GOD WE TRUST";
+		this.backMotto = "E PLURIBUS UNUM";
+		this.frontLabel = "LIBERTY";
+		this.backLabel = "UNITED STATES OF AMERICA";
+	}
+
+	public Coin(double value, int year)
+	{
 	}
 }
